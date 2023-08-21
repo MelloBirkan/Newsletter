df462aa3b47a31eaba9b4f003157e224cabbc52b
Mello Birkan <mgbirkan@icloud.com>
2023-08-21 16:39:22 -0300

    Arquivos do projeto estudando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f2dae7",
+        blue: {
+          light: "#c5dfff",
+          dark: "#061e3c",
+          hover: "#1057b0",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
